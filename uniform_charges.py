649843dd3953ea7aa8b2acdafa21ceb6aee27ba4
leriivt <90656798+leriivt@users.noreply.github.com>
2026-01-01 12:30:31 -0500

    updated comments more, reordered some things

--- uniform_charges.py
+++ uniform_charges.py
@@ -1,3 +1,4 @@
+#takes about 5 minutes to install
 import fmm3dpy as fmm
 import numpy as np
 import matplotlib as mpl
@@ -5,8 +6,8 @@ import matplotlib.pyplot as plt
 import time
 
 '''
-Given 1000 source charge electrons uniformly randomly distributed in a 1m x 1m area on the xy plane,
-use FMM to find electric field at 1000 uniformly randomly distributed target points in a 1m x 1m area on the xy plane
+Given 1e6 source charge electrons uniformly randomly distributed in a 1m x 1m area on the xy plane,
+use FMM to find electric field at 1e6 uniformly randomly distributed target points in a 1m x 1m area on the xy plane
 1m above the plane of electrons
 
 Plot electrons and electric field in 3d
@@ -14,22 +15,19 @@ Plot electrons and electric field in 3d
 '''
 skip = 1000
 
-eps = 10**(-5)
+eps = 10**(-4)
 e_charge = -1.6e-19 #charge of an electron in coulombs
 e_permitivity_vac = 8.854e-12
 k_e = 1 / 4 / np.pi / e_permitivity_vac #coulomb constant
 
-#setup for plotting in 3d
-fig = plt.figure(figsize=(10, 8))
-ax = fig.add_subplot(projection='3d')
+
 
 #generate source locations
-n = (int)(10e6) #1000 sources
+n = (int)(1e6) #1e6 sources
 sources = np.random.uniform(0,1,(3,n)) #generate x,y,z coords
 sources[2] = np.zeros(n) #set all the z coords to 0
 
-#plot electrons
-ax.scatter(sources[0][::skip], sources[1][::skip], sources[2][::skip], c='m')
+
 
 #setup charge values (all will have charge of 1 electron)
 charges = np.full(n, e_charge)
@@ -37,7 +35,7 @@ charges = np.full(n, e_charge)
 #print(charges)
 
 #generate target locations
-nt = n #1000 targets
+nt = n #1e6 targets
 targets = np.random.uniform(0,1,(3,nt)) #generate x,y,z coords
 #targets=np.array(sources)
 targets[2] = np.ones(nt) #set all the z coords to 1
@@ -46,12 +44,12 @@ targets[2] = np.ones(nt) #set all the z coords to 1
 #timer
 start_time = time.perf_counter()
 
-out = fmm.lfmm3d(eps=eps,sources=sources,charges=charges,targets=targets,pg=2,pgt=2)
+out = fmm.lfmm3d(eps=eps,sources=sources,charges=charges,targets=targets,pg=0,pgt=2)
 grad = out.gradtarg
-grad_s = out.grad
+#grad_s = out.grad
 #print(grad)
 e_field = grad * -k_e #scale gradient by coulomb constant
-e_field_s = grad_s * -k_e 
+#e_field_s = grad_s * -k_e 
 end_time = time.perf_counter()
 time_elapsed = end_time - start_time
 print("time to calculate e_field at targets: ")
@@ -73,8 +71,13 @@ norm = mpl.colors.Normalize(vmin=np.min(mag_e_field), vmax=np.max(mag_e_field))
 cmap = mpl.colormaps['viridis']
 colors = cmap(norm(mag_e_field))
 
+#setup for plotting in 3d
+fig = plt.figure(figsize=(10, 8))
+ax = fig.add_subplot(projection='3d')
+#plot electrons
+ax.scatter(sources[0][::skip], sources[1][::skip], sources[2][::skip], c='m')
 
-'''
+
 # Make the grid
 x, y, z = targets
 u, v, w = e_field
@@ -97,7 +100,7 @@ plt.show()
 #plot electric field magnitudes as 2d scatter plot
 fig, ax = plt.subplots(figsize=(8, 6))
 
-scatter = ax.scatter(targets[0], targets[1], c=mag_e_field, cmap='viridis')
+scatter = ax.scatter(targets[0][::skip], targets[1][::skip], c=mag_e_field[::skip], cmap='viridis')
 plt.colorbar(scatter, label='Electric Field [V/m]')
 
 ax.set_xlabel('X Pos [m]')
@@ -105,7 +108,7 @@ ax.set_ylabel('Y Pos [m]')
 
 plt.show()
 
-'''
+
 
 
 
